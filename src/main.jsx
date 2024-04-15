fb9658e06ddce3689fc5085bf6efb9306700c5f4
Maxrrrrr <pavliv.maxym@gmail.com>
2024-04-15 10:40:12 +0300

    czxczx

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,10 @@
-import React from 'react';
-import { Provider } from 'react-redux';
-import ReactDOM from 'react-dom/client';
-import 'modern-normalize';
-import { App } from './components/App.jsx';
-import './index.css';
-import { persistor, store } from './redux/store.js';
-import { PersistGate } from 'redux-persist/integration/react';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <App />
-      </PersistGate>
-    </Provider>
-  </React.StrictMode>
-);
+    <App />
+  </React.StrictMode>,
+)
